34751ed204f20ed9936c99c814e56e0353bf169a
Cezary <cezary.szukiel@gmail.com>
2023-05-07 20:39:57 +0200

    usunięto actual_date z l10 oraz import datetime

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+
 
 from django.shortcuts import render
 from django.views import View
@@ -7,5 +7,4 @@ from django.views import View
 class IndexView(View):
 
     def get(self, request):
-        ctx = {"actual_date": datetime.now()}
-        return render(request, "index.html", ctx)
+        return render(request, "index.html")
