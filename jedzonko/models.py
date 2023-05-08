d8ab3b989ef641e0ef49330f02c100ff451730f8
Cezary <cezary.szukiel@gmail.com>
2023-05-08 14:55:25 +0200

    models from 5.1 added (Plan, DayName, RecipePlan)

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -10,3 +10,23 @@ class Recipe(models.Model):
     updated = models.DateTimeField(auto_now=True)
     preparation_time = models.IntegerField()
     votes = models.IntegerField()
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    recipes = models.ManyToManyField(Recipe, through='RecipePlan')
+
+
+class DayName(models.Model):
+    name = models.CharField(max_length=64)
+    order = models.IntegerField(unique=True)
+
+
+class RecipePlan(models.Model):
+    meal_name = models.CharField(max_length=64)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    day_name = models.ForeignKey(DayName, on_delete=models.CASCADE)
