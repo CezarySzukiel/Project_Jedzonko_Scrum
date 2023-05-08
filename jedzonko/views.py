53138b25a1b2d05932f854712bfb70eca4965d4b
AleksanderJanusz <oleksanderjanusz@gmail.com>
2023-05-08 14:41:45 +0200

    feature-1.5

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,11 +1,23 @@
+import random
+
 from django.shortcuts import render
 from django.views import View
+from jedzonko.models import Recipe
 
 
 class IndexView(View):
 
     def get(self, request):
-        return render(request, "jedzonko/index.html")
+        recipes = []
+        for recipe in Recipe.objects.all():
+            recipes.append([recipe.name, recipe.description])
+        random.shuffle(recipes)
+
+        names = [i[0] for i in recipes[:3]]
+        descriptions = [i[1] for i in recipes[:3]]
+
+        return render(request, "jedzonko/index.html", {'names': names,
+                                                       'descriptions': descriptions})
 
 
 class Dashboard(View):
