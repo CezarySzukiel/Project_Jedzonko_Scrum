d6ac4411baf0156f4774fa6325cd7a7717637b46
Paweł Zwoliński <126811076+PawelZwo@users.noreply.github.com>
2023-05-09 09:17:58 +0200

    Revert "Feature 3.4"

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=255)
-    ingredients = models.TextField()
+    name = models.CharField(max_length=64)
+    # ingredients = models.ManyToManyField(Ingredients)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -13,19 +13,19 @@ class Recipe(models.Model):
 
 
 class Plan(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=64)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     recipes = models.ManyToManyField(Recipe, through='RecipePlan')
 
 
 class DayName(models.Model):
-    name = models.CharField(max_length=16)
+    name = models.CharField(max_length=64)
     order = models.IntegerField(unique=True)
 
 
 class RecipePlan(models.Model):
-    meal_name = models.CharField(max_length=255)
+    meal_name = models.CharField(max_length=64)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
     order = models.IntegerField()
