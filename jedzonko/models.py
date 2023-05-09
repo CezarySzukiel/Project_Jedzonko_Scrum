af1999b9dae0cc23b2cb78f05c91c4d897ffe2a5
Cezary <cezary.szukiel@gmail.com>
2023-05-09 09:31:37 +0200

    some bugs repaired

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=64)
-    # ingredients = models.ManyToManyField(Ingredients)
+    name = models.CharField(max_length=255)
+    ingredients = models.TextField()
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -12,19 +12,19 @@ class Recipe(models.Model):
 
 
 class Plan(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=255)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     recipes = models.ManyToManyField(Recipe, through='RecipePlan')
 
 
 class DayName(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=16)
     order = models.IntegerField(unique=True)
 
 
 class RecipePlan(models.Model):
-    meal_name = models.CharField(max_length=64)
+    meal_name = models.CharField(max_length=255)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
     order = models.IntegerField()
